4ecb79a035943166499abd4c702a81dfee990008
NeerajQA <neeraj1264qa@gmail.com>
2025-07-31 18:40:46 +0530

    .

--- api/index.js
+++ api/index.js
@@ -9,7 +9,6 @@ const productRoutes = require('./routes/productRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 const categoryRoutes = require('./routes/categoryRoutes');
 const customerdataRoutes = require('./routes/customerdataRoutes');
-const notificationRoutes = require('./routes/notificationRoutes');
 // Initialize the app
 const app = express();
 
@@ -27,6 +26,4 @@ app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/categories', categoryRoutes);
 app.use('/api/customerdata', customerdataRoutes);
-app.use('/api/notifications', notificationRoutes);
-
 module.exports = app;
